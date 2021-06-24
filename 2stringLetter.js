177f84c19c35edc754e77c70fb60372e8b830407
Nehanavgurukul <nehay19@navgurukul.org>
2021-06-24 10:20:45 +0530

    some new

--- 2stringLetter.js
+++ 2stringLetter.js
@@ -1,4 +1,5 @@
 //logical one ques here we have to find two starting and ending letter from a string?
+// if string is lenn than 2 then string and reverse string
 
 let readline = require("readline-sync");
 let str = readline.question("Enter the any string--");
@@ -9,8 +10,8 @@ if(str.length > 2){
     var j = 1
     let newstr1 = str[i]+str[j]
     let newstr2 = str[index2]+str[index1]
-    console.log(newstr1)
-    console.log(newstr2);
+    console.log(newstr1+newstr2)
+    // console.log(newstr2);
 }else{
-    console.log("string has very less length")
+    console.log(str+str[1]+str[0])
 }
